fee85bdb592311c54c7e1258f4141da901654e54
Tricia Brooker <grimmi5894@gmail.com>
2021-05-24 22:48:04 -0400

    created POST and saveNewHero controller

--- controllers/villains.js
+++ controllers/villains.js
@@ -12,9 +12,21 @@ const getVillainBySlug = async (request, response) => {
 
   return matchedVillain
     ? response.send(matchedVillain)
-    : response.sendStatus(404).send('Error-No Match Found')
+    : response.sendStatus(404)
+}
+
+const saveNewVillain = async (request, response) => {
+  const { name, movie, slug } = request.body
+
+  if (!name || !movie || !slug) {
+    return response.status(400).send('The following parameters are required: name, movie, slug')
+  }
+
+  const newVillain = await models.villains.create({ name, movie, slug })
+
+  return response.status(201).send(newVillain)
 }
 
 module.exports = {
-  getAllVillains, getVillainBySlug
+  getAllVillains, getVillainBySlug, saveNewVillain
 }
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-/* eslint-disable no-console */
 const express = require('express')
-const { getAllVillains, getVillainBySlug } = require('./controllers/villains')
+const bodyParser = require('body-parser')
+const { getAllVillains, getVillainBySlug, saveNewVillain } = require('./controllers/villains')
 
 const app = express()
 
@@ -8,6 +8,9 @@ app.get('/villains', getAllVillains)
 
 app.get('/villains/:slug', getVillainBySlug)
 
+app.post('/villains', bodyParser.json(), saveNewVillain)
+
 app.listen(1337, () => {
+  // eslint-disable-next-line no-console
   console.log('Listening on port 1337')
 })
