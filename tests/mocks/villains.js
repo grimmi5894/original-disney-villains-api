75425123ef3fdce9fb8602fe514972c5c204a60a
Tricia Brooker <grimmi5894@gmail.com>
2021-06-04 18:38:13 -0400

    added mock folder to test folder, created villains.js file in mocks, added  mock data to use for tests

---
+++ tests/mocks/villains.js
@@ -0,0 +1,53 @@
+const mockVillainsList = [{
+  id: 1,
+  name: 'Captain Hook',
+  movie: 'Peter Pan',
+  slug: 'captain-hook',
+  createdAt: '2021-05-24T17:45:01.000Z',
+  updatedAt: '2021-05-24T17:45:01.000Z',
+  deletedAt: null
+}, {
+  id: 2,
+  name: 'Cruella de Vil',
+  movie: 'One Hundred and One Dalmatians',
+  slug: 'cruella-de-vil',
+  createdAt: '2021-05-24T17:45:01.000Z',
+  updatedAt: '2021-05-24T17:45:01.000Z',
+  deletedAt: null
+}, {
+  id: 3,
+  name: 'Gaston',
+  movie: 'Beauty and the Beast',
+  slug: 'gaston',
+  createdAt: '2021-05-24T17:45:01.000Z',
+  updatedAt: '2021-05-24T17:45:01.000Z',
+  deletedAt: null
+}]
+
+const mockVillain = {
+  id: 4,
+  name: 'Hades',
+  movie: 'Hercules',
+  slug: 'hades',
+  createdAt: '2021-05-24T17:45:01.000Z',
+  updatedAt: '2021-05-24T17:45:01.000Z',
+  deletedAt: null
+}
+
+const mockPostVillainData = {
+  name: 'Red Skull',
+  movie: 'Captain America: The First Avenger',
+  slug: 'red-skull'
+}
+
+const mockPostVillainResponse = {
+  id: 5,
+  name: 'Red Skull',
+  movie: 'Captain America: The First Avenger',
+  slug: 'red-skull',
+  createdAt: '2021-05-24T17:45:01.000Z',
+  updatedAt: '2021-05-24T17:45:01.000Z',
+  deletedAt: null
+}
+
+module.exports = { mockVillainsList, mockVillain, mockPostVillainData, mockPostVillainResponse }
